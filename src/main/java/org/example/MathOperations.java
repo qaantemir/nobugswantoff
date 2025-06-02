f9c661371e839532d684d4b1ee6ce44bf80379f9
Kantemir Bifov <bkantemir1995@gmail.com>
2025-06-02 14:51:30 +0300

    fix hw 1

--- src/main/java/org/example/MathOperations.java
+++ src/main/java/org/example/MathOperations.java
@@ -5,7 +5,7 @@ public class MathOperations {
     public static int add(int x, int y) {return x + y;}
     public static int subtract(int x, int y) {return x - y;}
     public static int multyply(int x, int y) {return x * y;}
-    public static int divide(int x, int y) {return x / y;}
+    public static double divide(int x, int y) {return (double) x / y;}
 
     public static int findMax(int x, int y) {
         if (x > y) return x;
@@ -20,13 +20,13 @@ public class MathOperations {
 
     public static int squarePerimeter(int x) {return x+x+x+x;}
 
-    public static int convertSecondsToMinutes(int seconds) {return seconds / 60;}
+    public static double convertSecondsToMinutes(int seconds) {return (double) seconds / 60;}
 
     public static double averageSpeed(double distance, double time) {return distance / time;}
 
     public static double findHypotenuse(double a, double b) {return  Math.hypot(a, b);}
 
-    public static double circleCircumference(double radius) {return 2 * 3.14 * radius;}
+    public static double circleCircumference(double radius) {return 2 * Math.PI * radius;}
 
     public static double calculatePercentage(double total, double part) {return part / (total / 100);}
 
